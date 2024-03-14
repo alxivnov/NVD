6bac61a2cab7532c2838086a2517db35e7c2d13d
Alexander Ivanov <alex.p.ivanov@gmail.com>
2024-03-14 23:20:09 +0300

    Added downloader script

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
 # National Vulnerability Database downloader
 
-* To download particular CVE run:
+* To download particular CVE:
 ```shell
 ./nvd.sh API_KEY CVE_ID
 ```
 
-* To download full database run:
+* To download full database:
 ```shell
-./nvd.sh API_KEY [START_INDEX] [RESULTS_PER_PAGE] [SLEEP_SECONDS] [PAD_LENGTH]
+./nvd.sh API_KEY [START_INDEX] [RESULTS_PER_PAGE] [SLEEP_SECONDS] [PAD_TO_LENGTH]
 ```
 Arguments:
 Argument			| Description							| Default
 ------------------- | ------------------------------------- | -------
-START_INDEX			| Sync start index						| 0
-RESULTS_PER_PAGE	| Results per page						| 1000
-SLEEP_SECONDS		| Seconds to wait before the next page	| 6
-PAD_LENGTH			| Pad with zeroes to length				| 6
+START_INDEX			| Index of the first CVE to be returned	| 	    0
+RESULTS_PER_PAGE	| Number of CVE records to be returned	| 	 1000
+SLEEP_SECONDS		| Seconds to wait before the next page	| 	    6
+PAD_TO_LENGTH		| Pad with leading zeroes to length		| 	    6
---
+++ nvd.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+
+# https://nvd.nist.gov/developers/vulnerabilities
+# https://nvd.nist.gov/developers/api-workflows
+# ./nvd.sh ${API_KEY} ${startIndex} ${resultsPerPage} ${SLEEP_SECONDS}
+
+# chmod +x ./bin/nvd.sh
+
+
+API_KEY=$1
+
+if [[ $2 = CVE-* ]]; then
+	file="nvd-${2}.json"
+	url="https://services.nvd.nist.gov/rest/json/cves/2.0?cveId=${2}"
+
+	wget --header "apiKey: ${API_KEY}" -O "$file" "$url" || curl --header "apiKey: ${API_KEY}" --output "$file" "$url"
+else
+	startIndex=${2:-0}
+	resultsPerPage=${3:-1000}
+	SLEEP_SECONDS=${4:-6}
+	PAD_TO_LENGTH=${5:-6}
+
+	mkdir nvd || true
+
+	while
+		padded=$(printf "%0${PAD_TO_LENGTH}d" $startIndex)
+		file="./nvd/nvd-${padded}-${resultsPerPage}.json"
+		url="https://services.nvd.nist.gov/rest/json/cves/2.0?startIndex=${startIndex}&resultsPerPage=${resultsPerPage}"
+
+		echo "$url"
+		wget --header "apiKey: ${API_KEY}" -O "${file}" "${url}" || curl --header "apiKey: ${API_KEY}" --output "$file" "$url"
+		# echo "$file"
+	do
+		resultsPerPage=$(grep -oE '"resultsPerPage":\s*(\d+)' $file | sed -E 's/"resultsPerPage":\s*//g')
+		if [ $resultsPerPage -lt 1000 ]; then
+			mv $file "nvd-${startIndex}-${resultsPerPage}.json"
+			break
+		fi
+
+		if grep -q '"resultsPerPage":0,' $file; then
+			rm $file
+			break
+		fi
+
+		startIndex=$(($startIndex+$resultsPerPage))
+
+		sleep $SLEEP_SECONDS
+	done
+fi
